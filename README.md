b0b39510858f66fcf4586442e03f03d753e022b6
Nikita Serov <nikita-serov2003@yandex.ru>
2024-11-26 12:15:48 +0500

    added neo4j and depth for parsing

--- README.md
+++ README.md
@@ -17,18 +17,25 @@ pip install -r requirements.txt
 ```
 6. Запустить скрипт, передав необходимые параметры:
 ```bash
-python main.py <TOKEN> <ID> --output <FILENAME>
+python main.py --token <TOKEN> --user_id <ID> --query <TYPE> --depth <INT> --neo4j_uri <URI> --neo4j_login <LOGIN> --neo4j_pass <PASSWORD> --clear
 ```
 
 **Параметры:**
+--token — access token VK.
+--user_id — ID пользователя VK.
+--query — (необязательный) Тип запроса для выполнения в базе данных. Возможные значения: users_count, groups_count, top_users, top_groups, mutual_followers.
+--depth — (необязательный) Глубина извлечения информации о пользователе (по умолчанию 2).
+--neo4j_uri — (необязательный) URI для подключения к базе данных Neo4j (по умолчанию neo4j://localhost:7687).
+--neo4j_login — (необязательный) Логин для подключения к базе данных Neo4j (по умолчанию neo4j).
+--neo4j_pass — (необязательный) Пароль для подключения к базе данных Neo4j (по умолчанию 11111111).
+--clear — (необязательный) Флаг для очистки базы данных перед запуском.
 
-- `<TOKEN>` — VK API токен.
-- `<ID>` — ID пользователя VK.
-- `--output` — необязательный параметр. Имя файла для сохранения результата (по умолчанию `output.json`).
-
-### Пример:
+### Примеры:
 ```bash
-python main.py vk1.a.abc 1 --output user_data.json
+python main.py --token vk1.a.abc --user_id 1 --query users_count
 ```
-
-Этот пример извлечет данные о пользователе с ID `1` и сохранит их в файл `user_data.json`.
+выведет количество пользователей в бд
+```bash
+python main.py --token vk1.a.abc --user_id 1 --clear --depth 3
+```
+очистит бд, сгенерирует значения с глубиной 3 и запишет их в бд
--- main.py
+++ main.py
@@ -1,67 +1,206 @@
-import requests
-import json
 import argparse
+import json
+import logging
+import requests
+from neo4j import GraphDatabase
 
-VK_API_VERSION = "5.199"
 
-def get_user_info(token, user_id):
-    base_url = "https://api.vk.com/method/"
+class Neo4jHandler:
+    def __init__(self, uri, user, password):
+        self.driver = GraphDatabase.driver(uri, auth=(user, password))
 
-    user_info_url = f"{base_url}users.get"
+    def close(self):
+        self.driver.close()
+
+    def run_query(self, query, parameters=None):
+        with self.driver.session() as session:
+            result = session.run(query, parameters)
+            return [record for record in result]
+
+    def create_user(self, user_data):
+        query = """
+        MERGE (u:User {id: $id})
+        SET u.name = $name, u.screen_name = $screen_name, u.sex = $sex, u.city = $city
+        """
+        self.run_query(query, user_data)
+
+    def create_group(self, group_data):
+        query = """
+        MERGE (g:Group {id: $id})
+        SET g.name = $name, g.screen_name = $screen_name
+        """
+        self.run_query(query, group_data)
+
+    def rel_follow(self, user_from, user_to):
+        query = """
+        MATCH (u1:User {id: $from})
+        MATCH (u2:User {id: $to})
+        MERGE (u1)-[:FOLLOWS]->(u2)
+        """
+        self.run_query(query, {'from': user_from, 'to': user_to})
+
+    def rel_sub(self, user, group):
+        query = """
+        MATCH (u:User {id: $user})
+        MATCH  (g:Group {id: $group})
+        MERGE (u)-[:SUBSCRIBED]->(g)
+        """
+        self.run_query(query, {'user': user, 'group': group})
+        
+    def clear_db(self):
+        query = """
+        MATCH (n)
+        DETACH DELETE n
+        """
+        self.run_query(query)
+            
+    def query(self, query_type):
+        queries = {
+            'users_count': "MATCH (u:User) RETURN COUNT(u) AS count",
+            'groups_count': "MATCH (g:Group) RETURN COUNT(g) AS count",
+            'top_users': """
+                MATCH (u:User)<-[:FOLLOWS]-()
+                RETURN u.id, u.name, COUNT(*) AS followers_count
+                ORDER BY followers_count DESC LIMIT 5
+            """,
+            'top_groups': """
+                MATCH (g:Group)<-[:SUBSCRIBED]-()
+                RETURN g.id, g.name, COUNT(*) AS subscribers_count
+                ORDER BY subscribers_count DESC LIMIT 5
+            """,
+            'mutual_followers': """
+                MATCH (u1:User)-[:FOLLOWS]->(u2:User)-[:FOLLOWS]->(u1) 
+                RETURN u1.id, u2.id
+            """
+        }
+    
+        try:
+            result = self.run_query(queries[query_type])
+            return result
+        except KeyError as ke:
+            logger.error(f"такого запроса нет")
+            return []      
+    
+def get_user_info(args, neo4j_handler, user_id, depth=2):
+    logger.info(f'fetching info for {user_id}')
+    
+
+    user_info_url = f'https://api.vk.com/method/users.get'
     user_info_params = {
-        "user_ids": user_id,
-        "fields": "followers_count",
-        "access_token": token,
-        "v": VK_API_VERSION
+        'user_ids': user_id,
+        'fields': 'city,sex,screen_name',
+        'access_token': args.token,
+        'v': '5.199'
     }
-    user_info_response = requests.get(user_info_url, params=user_info_params)
-    user_info = user_info_response.json().get("response", [{}])[0]
+    user_response = requests.get(user_info_url, params=user_info_params)
+    user_data = user_response.json()    
+    if 'error' in user_data:
+        error_msg = user_data['error'].get('error_msg', 'Unknown error')
+        logger.error(f"Error fetching user info: {error_msg}")
+        return
+    if len(user_data['response']) == 0:
+        logger.error(f"No info: {user_response.text}")
+        return
 
-    followers_url = f"{base_url}users.getFollowers"
+    user = user_data['response'][0]
+
+    user_dict = {
+        'id': user['id'],
+        'name': user['first_name'] + ' ' + user['last_name'],
+        'screen_name': user.get('screen_name', ''),
+        'sex': user.get('sex', ''),
+        'city': user.get('city', {}).get('title', '') if user.get('city') else ''
+    }
+    logger.info(f"user: {user_dict}")
+    neo4j_handler.create_user(user_dict)
+
+    followers_url = f'https://api.vk.com/method/users.getFollowers'
     followers_params = {
-        "user_id": user_id,
-        "count": 1000,
-        "access_token": token,
-        "v": VK_API_VERSION
+        'user_id': user['id'],
+        'access_token': args.token,
+        'v': '5.199'
     }
+
     followers_response = requests.get(followers_url, params=followers_params)
-    followers = followers_response.json().get("response", {}).get("items", [])
+    followers_data = followers_response.json()
+    followers = followers_data.get('response', {}).get('items', [])
 
-    subscriptions_url = f"{base_url}users.getSubscriptions"
+    subscriptions_url = f'https://api.vk.com/method/users.getSubscriptions'
     subscriptions_params = {
-        "user_id": user_id,
-        "access_token": token,
-        "v": VK_API_VERSION
+        'user_id': user['id'],
+        'extended': 1,
+        'access_token': args.token,
+        'v': '5.199'
     }
+
     subscriptions_response = requests.get(subscriptions_url, params=subscriptions_params)
-    subscription_ids = subscriptions_response.json().get("response", {}).get("groups", {}).get("items", [])
+    subscriptions_data = subscriptions_response.json()
+    groups = subscriptions_data.get('response', {}).get('items', [])
 
-    groups_url = f"{base_url}groups.getById"
-    groups_params = {
-        "group_ids": ','.join(map(str, subscription_ids)),
-        "access_token": token,
-        "v": VK_API_VERSION
-    }
-    groups_response = requests.get(groups_url, params=groups_params)
-    subscription_groups = groups_response.json().get("response", [])
+    for follower in followers:
+        logger.info(f"follower: {follower}")
+        neo4j_handler.rel_follow(follower, user['id'])
+        if depth > 1:
+            get_user_info(args, neo4j_handler, follower, depth - 1)
 
-    return {
-        "user_info": user_info,
-        "followers": followers,
-        "subscriptions": subscription_groups
-    }
+    for group in groups:
+        if group['type'] == 'profile':
+            continue
 
-def save_data_to_json(token, user_id, filename):
-    data = get_user_info(token, user_id)
-    with open(filename, 'w', encoding='utf-8') as f:
-        json.dump(data, f, ensure_ascii=False, indent=4)
-    print(f"Data has been saved to {filename}")
-
+        group_data = {
+            'id': group['id'],
+            'name': group['name'],
+            'screen_name': group.get('screen_name', '')
+        }
+        neo4j_handler.create_group(group_data)
+        neo4j_handler.rel_sub(user['id'], group['id'])
+    
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Fetch VK user data and save it to a JSON file.")
-    parser.add_argument("token", help="VK API access token")
-    parser.add_argument("user_id", help="VK user ID")
-    parser.add_argument("--output", default="output.json", help="Output filename (default: output.json)")
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--user_id', type=str, help='ID VK user')
+    parser.add_argument('--token', type=str, help='Access token VK')
+    parser.add_argument('--query', type=str, help='users_count, groups_count, top_users, top_groups, mutual_followers')
+    parser.add_argument('--depth', type=int, help='fetching depth')
+    parser.add_argument('--neo4j_uri', type=str, default='neo4j://localhost:7687', help='URI neo4j')
+    parser.add_argument('--neo4j_login', type=str, default='neo4j',help='login neo4j')
+    parser.add_argument('--neo4j_pass', type=str, default='11111111',help='password neo4j')
+    parser.add_argument('--clear', action='store_true', help='clear database')
 
     args = parser.parse_args()
-    save_data_to_json(args.token, args.user_id, args.output)
+
+    logging.basicConfig(level='INFO', format='%(asctime)s [%(levelname)s]: %(message)s')
+    logger = logging.getLogger(__name__)
+
+    neo4j_handler = Neo4jHandler(args.neo4j_uri, args.neo4j_login, args.neo4j_pass)
+
+    if args.query:
+        result = neo4j_handler.query(args.query)
+        
+        if args.query in ['users_count', 'groups_count']:
+            print(result[0]['count'])
+        elif args.query == 'top_users':
+            if result:
+                for record in result:
+                    print(f"{record['u.name']}, followers: {record['followers_count']}")
+            else:
+                print("not found")
+        elif args.query == 'top_groups':
+            if result:
+                for record in result:
+                    print(f"{record['g.name']}, subscribers: {record['subscribers_count']}")
+            else:
+                print("not found")
+        elif args.query == 'mutual_followers':
+            if result:
+                for record in result:
+                    print(f"{record['u1.id']} - {record['u2.id']}")
+            else:
+                print("not found")
+    else:
+        if args.clear:
+            logger.info("clearing db...")
+            neo4j_handler.clear_db()
+        get_user_info(args, neo4j_handler, args.user_id, depth=args.depth)
+
+    neo4j_handler.close()
